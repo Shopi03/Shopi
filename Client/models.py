d7b5e199f9589b2bb6da7ce7a7cb964d6ba65b90
Camara <shopi03.gn@gmail.com>
2025-10-26 21:29:11 +0000

    creation des models à part partenaire et administrateur et la manupilation de entreprise, ajout, modification

--- Client/models.py
+++ Client/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Client(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField()
+    telephone = models.CharField(max_length=15)
+    date_inscription = models.DateField(auto_now_add=True)
+    actif = models.BooleanField(default=True)
+    password = models.CharField(max_length=128)
+    date_naissance = models.DateField(null=True, blank=True)
+    sexe = models.CharField(max_length=10, choices=[('homme', 'Homme'), ('femme', 'Femme')])
+    profil = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    def __str__(self):
+        return f" {self.nom} {self.prenom}"
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
--- Livreur/models.py
+++ Livreur/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Livreur(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    matricule = models.CharField(max_length=20)
+    email = models.EmailField()
+    telephone = models.CharField(max_length=15)
+    date_embauche = models.DateField(auto_now_add=True)
+    actif = models.BooleanField(default=True)
+    password = models.CharField(max_length=128)
+    date_naissance = models.DateField(null=True, blank=True)
+    sexe = models.CharField(max_length=10, choices=[('homme', 'Homme'), ('femme', 'Femme')])
+    profil = models.ImageField(upload_to='livreurs_profiles/', null=True, blank=True)
+    vehicule = models.CharField(max_length=100)
+    zone_livraison = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.nom} {self.prenom}"
+    class Meta:
+        verbose_name = "Livreur"
+        verbose_name_plural = "Livreurs"
 # Create your models here.
